8929c7c2d26d75e1ec5a5d1947bcfcd7bb7d95d6
HecatesRevenge <87406020+HecatesRevenge@users.noreply.github.com>
2026-04-02 15:40:05 -0400

    Intial Commit
    This is the basis of our new project and were all page rewrites will go

---
+++ Plenty_of_Finch/Plenty_of_Finch/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Plenty_of_Finch.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
