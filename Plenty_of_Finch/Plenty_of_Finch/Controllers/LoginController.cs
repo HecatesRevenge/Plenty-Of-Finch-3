a5148b9129ee55c270019e055f1a938c849c66cd
HecatesRevenge <87406020+HecatesRevenge@users.noreply.github.com>
2026-04-07 17:27:40 -0400

    Finished Coupling Login Page
    The Login page now works with existing file structure. Also renamed 'DatabaseStuff.cs" to LoginDatabaseConnection

--- Plenty_of_Finch/Plenty_of_Finch/Controllers/LoginController.cs
+++ Plenty_of_Finch/Plenty_of_Finch/Controllers/LoginController.cs
@@ -1,27 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Plenty_of_Finch.DBConnections;
 
-namespace apiTesting.Controllers
+namespace Plenty_of_Finch.Controllers
 {
     public class LoginController : Controller
     {
-        public IActionResult Index2()
-        {
-            return View();
-        }
         [HttpGet]
-        public IActionResult Index2(int x, int y)
+        public IActionResult Login()
         {
-            int z = x + y;
-            ViewBag.numbers = z;
             return View();
         }
-        [HttpGet("Login/Index2Check")]
-        public IActionResult Index2(string username, string password)
+        
+
+        //What does this code do? 
+        [HttpGet("Login/Math")]
+        public IActionResult Math(int x, int y)
+        {
+            ViewBag.numbers = x + y;
+            return View("Login");
+        }
+
+        [HttpGet("Login/LoginCheck")]
+        public IActionResult LoginCheck(string username, string password)
         {
             Databasestuff db = new Databasestuff();
             int trueorfalse = db.checkLogin(username, password);
-            ViewBag.check = trueorfalse;    
-            return View();
+            ViewBag.check = trueorfalse;
+            return View("Login");
         }
     }
 }
--- Plenty_of_Finch/Plenty_of_Finch/obj/Debug/net10.0/Plenty_of_Finch.AssemblyInfo.cs
+++ Plenty_of_Finch/Plenty_of_Finch/obj/Debug/net10.0/Plenty_of_Finch.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Plenty_of_Finch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acb3da8de1eb656a473335356880a4ba88297a13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ac192cd7a06adeb559729d47323d4660a1dd800")]
 [assembly: System.Reflection.AssemblyProductAttribute("Plenty_of_Finch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Plenty_of_Finch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
